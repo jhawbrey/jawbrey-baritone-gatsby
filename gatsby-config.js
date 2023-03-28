de7b13eedf8323a047a689d33973d1da139e92b8
Jason Awbrey <jawbrey@gmail.com>
2023-03-27 21:49:53 -0500

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-319662-4",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
