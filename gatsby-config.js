6a193a616517549ac6972e622cec5a78d90a9f0f
Jason Awbrey <jawbrey@gmail.com>
2023-03-27 21:24:45 -0500

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,30 @@ module.exports = {
         icon: `src/images/ja.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-319662-4",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "jasonawbrey.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
